c1e8768b3c572c4837eea4a565d56e5fc198a1d9
Jakub Kołodziej <kolomakuba@gmail.com>
2018-07-17 22:26:36 +0200

    Core Data model

---
+++ FrRunner/Functionalities/Base/BaseViewController.swift
@@ -0,0 +1,24 @@
+//
+//  BaseViewController.swift
+//  FrRunner
+//
+//  Created by Jakub Kołodziej on 17.07.2018.
+//  Copyright © 2018 Jakub Kołodziej. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class BaseViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
--- FrRunner/Functionalities/Login/LoginScreenViewController.swift
+++ FrRunner/Functionalities/Login/LoginScreenViewController.swift
@@ -22,5 +22,12 @@ class LoginScreenViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    
+ 
+    @IBAction func showBaseViewController(_ sender: Any) {
+        
+        let mainStoryboard : UIStoryboard = UIStoryboard(name: "BaseView", bundle: nil)
+        let vc : UIViewController = mainStoryboard.instantiateViewController(withIdentifier: "BaseView")
+        self.present(vc, animated: true, completion: nil);
+       
+    }
 }
